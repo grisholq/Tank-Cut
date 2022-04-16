4c453ddddcd18913438cd1b29ae8536289f2d4c4
grisholq <drowen2014@mail.ru>
2022-04-16 10:15:15 +0300

    Попытка исправить 7

--- Assets/Core/Common/Patrol/PatrolRawPoints.cs
+++ Assets/Core/Common/Patrol/PatrolRawPoints.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using System;
 
+[Serializable]
 public struct PatrolRawPoints
 {
     public Transform[] RawPoints;
---
+++ Assets/Core/Converters/PatrolPointsProvider.cs
@@ -0,0 +1,6 @@
+using Voody.UniLeo;
+
+public class PatrolPointsProvider : MonoProvider<PatrolRawPoints>
+{
+
+}
